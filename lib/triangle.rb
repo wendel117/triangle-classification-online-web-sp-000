82051d08eae5b5ddc3ac61df80b7d32caa902344
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-27 15:15:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,4 @@
 
-class TriangleError < StandardError
-  def message
-    "Action Bronson, No matter what, you're going to get a comparison to something. I Gave You A Grace Period, but that SportsNation triangular mentioning of my name was an ERROR!"
-  end
-end
 
 class Triangle
   def initialize(side1, side2, side3)
@@ -11,7 +6,11 @@ class Triangle
   @side2 = side2
   @side3 = side3
 end
-
+class TriangleError < StandardError
+  def message
+    "Action Bronson, No matter what, you're going to get a comparison to something. I Gave You A Grace Period, but that SportsNation triangular mentioning of my name was an ERROR!"
+  end
+end
 def kind
   if @side1 == 0 || @side2 == 0 || @side3 == 0
     raise TriangleError
